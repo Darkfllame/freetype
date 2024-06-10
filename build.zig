be03df0d15b7f934d6f6ce22b5570d5fcd38d7ef
Darkfllame <ntmconar5@gmail.com>
2024-06-10 15:06:01 +0200

    updated to work with zig 0.13.0

--- build.zig
+++ build.zig
@@ -31,7 +31,7 @@ pub fn build(b: *std.Build) void {
     lib.defineCMacro("HAVE_UNISTD_H", "1");
     lib.addCSourceFiles(.{ .files = &sources, .flags = &.{} });
     if (target.result.os.tag == .macos) lib.addCSourceFile(.{
-        .file = .{ .path = "src/base/ftmac.c" },
+        .file = b.path("src/base/ftmac.c"),
         .flags = &.{},
     });
     lib.installHeadersDirectory(b.path("include/freetype"), "freetype", .{});
